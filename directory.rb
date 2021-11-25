121c578a6ae239b440b7c47e5fbe6e09c83b5ef3
Ben Coyne <bencoyne123@yahoo.co.uk>
2021-11-25 12:24:59 +0000

    Refactor directory.rb - added interactive menu

--- directory.rb
+++ directory.rb
@@ -1,22 +1,51 @@
-# We first create some methods to help take and display:
+@students = [] # an empty array accessible to all methods
 
 def insert_student
-  # Prompt the user for the name of the student they wish to add.
-  puts "Enter Student's name: "
+  puts "Enter the name of the student: "
   puts "To finish, just hit return twice."
-  # Create an empty array.
-  students = []
   # Get the first name.
   name = gets.chomp
   # While the name is not empty, repeat this code.
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    # add the student hash to the @students array.
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     # Get another name from the user.
+    puts "Enter Student's name or hit return to finish: "
     name = gets.chomp
   end
-  # return the students array.
-  students
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      insert_student
+    when "2"
+      show_students
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you mean, try again."
+  end
 end
 
 def print_header
@@ -24,18 +53,16 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  students.each do |student| 
+def print_students_list
+  @students.each do |student| 
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students."
 end
 
 # Nothing happens until we call out methods.
-students = insert_student
-print_header
-print(students)
-print_footer(students)
+
+interactive_menu
