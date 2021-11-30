cf1d4b5adc1861b994dfaddea8e99bc6b4301418
Ben Coyne <bencoyne123@yahoo.co.uk>
2021-11-30 16:15:13 +0000

    Created sort_students function, edited print_menu and process methods

--- directory.rb
+++ directory.rb
@@ -14,22 +14,22 @@ def insert_student
     @age = STDIN.gets.chomp.to_i
     puts "Enter student's location:"
     @location = STDIN.gets.chomp
-	puts "Enter cohort:"
+	  puts "Enter cohort:"
     @cohort = STDIN.gets.chomp
-	if @cohort == ""
-		@cohort = :December
+	  if @cohort == ""
+		  @cohort = :December
 	end
 	add_student
-    puts "We now have #{@students.count} students."
-    puts "Input the name of another student or hit Enter to finish "
-    print ">> "
-    @name = STDIN.gets.chomp
+  puts "We now have #{@students.count} students."
+  puts "Input the name of another student or hit Enter to finish "
+  print ">> "
+  @name = STDIN.gets.chomp
   end  
   @students
 end
 
 def add_student
-  @students << {name: @name, cohort: @cohort, age: @age, location: @location}
+  @students << {name: @name, age: @age, location: @location, cohort: @cohort}
 end
 
 def interactive_menu
@@ -44,6 +44,7 @@ def print_menu
   puts "2. Show the students"
   puts "3. Save students to students.csv"
   puts "4. Load students from students.csv"
+  puts "5. Sort students by info"
   puts "9. Exit"
   print ">> "
 end
@@ -64,6 +65,8 @@ def process(selection)
       save_students
     when "4"
       load_students
+    when "5"
+      sort_students
     when "9"
       exit # this will cause the program to terminate
     else
@@ -78,7 +81,7 @@ end
 
 def print_students
   @students.each_with_index do |student, index|
-    puts "#{index + 1}) #{student[:name]} | #{student[:age]} years old | Location: #{student[:location]} |   (#{student[:cohort]} cohort)"
+    puts "#{index + 1}) #{student[:name]} | #{student[:age]} years old | Location: #{student[:location]} | (#{student[:cohort]} cohort)"
   end
 end
 
@@ -87,7 +90,7 @@ def print_footer
   if @students.count == 0
     puts "We currently have no students enrolled\n"
   elsif @students.count == 1
-      puts "Overall, we have #{@students.count} great student\n"
+    puts "Overall, we have #{@students.count} great student\n"
   else
     puts "Overall, we have #{@students.count} great students\n"
   end
@@ -108,7 +111,7 @@ end
 def load_students(filename = "students.csv")
   file = File.open(filename, "r")
   file.readlines.each do |line|
-	@name, @cohort, @age, @location = line.chomp.split(',')
+	@name, @age, @location, @cohort, = line.chomp.split(',')
 	add_student
   end
   puts "Loaded #{@students.count} students from #{filename}"
@@ -124,6 +127,16 @@ def try_load_students
     puts "Sorry... #{filename} does not exist."
     exit # quit the program
   end 
+end
+
+def sort_students
+  puts "Sort students by: name, age, location or cohort" 
+  print ">> "
+  sorter = gets.chomp.to_sym
+    if sorter.empty?
+      sort_students
+    end
+  students = @students.sort_by! { |student| student[sorter] }
 end
 
 try_load_students
